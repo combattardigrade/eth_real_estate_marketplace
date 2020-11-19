6328ad42022d3bb2b8eff07b4d4d1153653c7ec3
Combat Tardigrade <combattardigrade@gmail.com>
2020-11-19 00:24:15 -0600

    ownable

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -9,16 +9,35 @@ import "./Oraclize.sol";
 contract Ownable {
     //  TODO's
     //  1) create a private '_owner' variable of type address with a public getter function
+    address private _owner;
+
+    function getOwner() {
+        return _owner;
+    }
+
     //  2) create an internal constructor that sets the _owner var to the creater of the contract 
+    constructor() {
+        _owner = msg.sender;
+    }
+
     //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
+    modifier onlyOwner {
+        require(msg.sender == _owner, "Ownable: Only the owner can access this feature");
+        _;
+    }
+
     //  4) fill out the transferOwnership function
     //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
 
     function transferOwnership(address newOwner) public onlyOwner {
         // TODO add functionality to transfer control of the contract to a newOwner.
         // make sure the new owner is a real address
-
+        require(newOwner != address(0), "Ownable: Invalid address");
+        _owner = newOwner;
+        emit TransferOwnership(newOwner);
     }
+
+    event TransferOwnership(address newOwner);
 }
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
